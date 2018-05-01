72945fbca3e1edfa5c7cbb1f101cc496688fffc3
Brettink <brett_wad_12@hotmail.com>
2018-05-01 16:28:27 -0600

    Test Conflicts

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
     Text hi;
     Text healthtext;
+    Text otherText;
     int health = 0;
     
 	// Use this for initialization
 	void Start () {
         healthtext = GameObject.FindGameObjectWithTag("healthtext").GetComponent<Text>();
+        otherText = GameObject.FindGameObjectWithTag("blah").GetComponent<Text>();
+        otherText.text = "HI THERE";
     }
 	
 	// Update is called once per frame
