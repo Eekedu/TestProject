cd91b8b44ee3de9e0594fdf8ee05de9b53ce0a7d
Kyttynz Tehkat <kyttynz@gmail.com>
2018-05-01 16:29:55 -0600

    Health increases only every second.

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -11,11 +11,18 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         healthtext = GameObject.FindGameObjectWithTag("healthtext").GetComponent<Text>();
+        fHealthNextThink = Time.fixedTime + 1.0f;
     }
-	
+
+    float fHealthNextThink;
 	// Update is called once per frame
 	void Update () {
-        health++;
+        if (Time.fixedTime >= fHealthNextThink)
+        {
+            health+=10;
+            fHealthNextThink = Time.fixedTime + 1.0f;
+            Debug.Log("Thinked");
+        }
         healthtext.text = "Health: " + health;
 	}
 }
